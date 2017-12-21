2d1c0f99d59822f5c153a04b04e85c3cfc21c02f
BrunoGomesCoelho <bruno.gomes.coelho@usp.br>
2017-12-20 23:14:44 -0200

    Add day3

--- day3/puzzle1.py
+++ day3/puzzle1.py
@@ -1,4 +1,58 @@
+"""
+The resolution bases itself on the fact that given a number X:
+-- First find out the highest odd numbers "corner" such that corner**2 <= X
+That is, a number that is in the diagonal that connects 1 to 9 to 25 to 49....
+-- Then we know that the move list so far will be:
+    R 1U 2L 2D 2R
+    R 3U 4L 4D 4R
+    R 5U 6L 6D 6R
+    ....
+    .
+    .
+    R (corner-2)*U  (corner-1)*L  (corner-1)*D  (corner-1)*R
+
+We can see that after a full cycle, we have 1R + 1U
+so after cicles = corner // 2, we would have cicles*(1R + 1U) moves.
+
+then find how many steps are missing from our original number
+(X - corner**2) and go through our move list moving as needed, making sure to
+cancel a L with a R/U with a D and vice-versa.
+"""
+
+
+from math import sqrt
 
 
 
+def process_move(move, u, l, r, d):
+    return {
+            "U": lambda: (u+1, l, r, d) if d == 0 else (u, l, r, d-1),
+            "L": lambda: (u, l+1, r, d) if r == 0 else (u, l, r-1, d),
+            "R": lambda: (u, l, r+1, d) if l == 0 else (u, l-1, r, d),
+            "D": lambda: (u, l, r, d+1) if u == 0 else (u-1, l, r, d),
+        }.get(move, lambda: print("This is a bug! Missing operation in dict"))()
+
+
+# Our input
+# x = 1024
+x = 361527
+
+corner = int(sqrt(x))
+
+if corner % 2 == 0:
+    corner -= 1
+cicles = corner // 2
+
+
+# counts for each direction
+u = l = 0
+r = d = cicles
+
+move_list = ["R"] + (corner)*["U"] + (corner+1)*["L"] + (corner+1)*["D"] + (corner+1)*["R"]
+
+
+for move in move_list[0: (x-corner**2)]:
+    u, l, r, d = process_move(move, u, l, r, d)
+
+print(u + l + r + d)
 
---
+++ day3/puzzle2.py
@@ -0,0 +1,48 @@
+"""
+For this one we use a completely different stategy: actually create
+the matrix and populate it
+"""
+
+def process_move(move, x, y):
+    return {
+            "U": lambda: (x, y-1),
+            "L": lambda: (x-1, y),
+            "R": lambda: (x+1, y),
+            "D": lambda: (x, y+1),
+        }.get(move, lambda: print("This is a bug! Missing operation in dict"))()
+
+
+def check_adjacents_squares(x, y):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            count += matrix[y+i][x+j]
+            # print("matrix[%d][%d] = %d" % (y+i, x+j, matrix[y+i][x+j]))   
+    return count
+
+
+# puzzle_input = 310
+puzzle_input = 361257
+limit = 1000    # since 1000**2 >> puzzle_input
+
+x, y = int(limit/2), int(limit/2)
+matrix = [ limit*[0] for _ in range(limit) ]
+
+matrix[y][x] = 1
+current_total = 1
+corner = 1
+
+while current_total < puzzle_input:
+    move_list = ["R"] + (corner)*["U"] + (corner+1)*["L"] + (corner+1)*["D"] + (corner+1)*["R"]
+    for move in move_list:
+        new_x, new_y = process_move(move, x, y)
+        matrix[new_y][new_x] = current_total = check_adjacents_squares(new_x, new_y)
+        x, y = new_x, new_y
+        
+        # print(current_total, "matrix[%d][%d] = %d" % (new_y, new_x, matrix[new_y][new_x]))
+        if current_total > puzzle_input:
+            break
+        print(current_total)
+    corner += 2
+
+print(current_total)
